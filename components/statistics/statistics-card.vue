a9b2d206427478d5b9b4e1515c6b0b3f910a2b8b
victor-rioba <riobavictor2019@gmail.com>
2025-06-01 09:28:17 +0300

    feat: statistics section

---
+++ components/statistics/statistics-card.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  value: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="flex flex-col bg-darker border border-secondary-mid p-5 rounded-md"
+  >
+    <span class="text-gray font-semibold">{{ label }}</span>
+    <span class="text-[32px] font-normal font-mono">{{ value }}</span>
+  </div>
+</template>
---
+++ components/statistics/statistics.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const statistics = [
+  { label: "Mission Count", value: "274" },
+  { label: "Mission Success", value: "93.5%" },
+  { label: "Mission Savings", value: "$5,480" },
+];
+</script>
+
+<template>
+  <div class="flex gap-5">
+    <StatisticsCard
+      v-for="statistic in statistics"
+      :key="statistic.label"
+      :label="statistic.label"
+      :value="statistic.value"
+      class="w-1/3"
+    />
+  </div>
+</template>
