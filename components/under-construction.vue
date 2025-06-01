d89a3db94181ba74f060f9cf49af3dc03fc0829d
victor-rioba <riobavictor2019@gmail.com>
2025-06-01 09:25:47 +0300

    feat: create sub-pages and show 'Under construction'

---
+++ components/under-construction.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid place-content-center">
+    <div class="l text-9xl translate-x-12 translate-y-4">
+      <svg
+        class="animate-spin-slow"
+        width="1em"
+        height="1em"
+        viewBox="0 0 717 717"
+      >
+        <path
+          fill="currentColor"
+          d="m649 143l-68 69c18 28 31 60 37 94h99v106h-99c-6 34-19 65-37 93l68 70l-75 75l-69-69c-28 18-59 32-93 38v98H305v-98c-34-6-65-20-93-38l-70 69l-74-75l68-70c-18-28-32-59-38-93H0V306h98c6-34 20-66 38-94l-68-69l74-75l70 69c28-18 59-32 93-38V0h107v99c34 6 65 20 93 38l69-69zM358 507c82 0 149-66 149-148s-67-149-149-149s-147 67-147 149s65 148 147 148z"
+        ></path>
+      </svg>
+    </div>
+
+    <div class="s text-8xl">
+      <svg
+        class="animate-spin-reverse"
+        width="1em"
+        height="1em"
+        viewBox="0 0 512 512"
+      >
+        <path
+          d="M448 294.4v-76.8h-42.8c-3.4-14.4-8.9-28-16.1-40.5l29.8-29.7-54.3-54.3-29.1 29.1c-12.6-7.7-26.4-13.5-41.1-17.3V64h-76.8v40.9c-14.7 3.8-28.5 9.7-41.1 17.3l-29.1-29.1-54.3 54.3 29.8 29.7c-7.2 12.5-12.6 26.1-16.1 40.5H64v76.8h44.1c3.8 13.7 9.5 26.6 16.7 38.6l-31.7 31.7 54.3 54.3 32.3-32.3c11.7 6.8 24.5 11.9 37.9 15.4v46h76.8v-46c13.5-3.5 26.2-8.6 37.9-15.4l32.3 32.3 54.3-54.3-31.6-31.7c7.2-11.9 12.9-24.8 16.7-38.6h44zm-192 15.4c-29.7 0-53.7-24.1-53.7-53.8s24-53.8 53.7-53.8 53.8 24.1 53.8 53.8-24.1 53.8-53.8 53.8z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </div>
+
+    <div class="text-xl text-center font-bold pt-5">Under Construction...</div>
+  </div>
+</template>
